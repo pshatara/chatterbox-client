fd77cb825e9e71e895722f20965f39c5761d72bc
pshatara <pshatara@gmail.com>
2014-09-22 15:38:51 -0700

    got some basics done

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,2 +1,56 @@
 // YOUR CODE HERE:
+  var app = {
+    init: function() {
+      app.handleSubmit();
+    },
+    send: function(message) {
+      $.ajax({
+        type: 'POST',
+        url: 'localhost',
+        data: JSON.stringify(message),
+        success: function(message) {},
+        // dataType: jsonp
+      });
+    },
+    fetch: function() {
+      $.ajax({});
+    },
+    clearMessages: function() {
+      $('#chats').empty();
+    },
+    addMessage: function(message) {
+      $('#chats').append("<p><span class='username'>"+message.username
+        +": </span>"+message.text+"</p>");
+      // this.send(message);
+      $('.username').click(function() {
+        // console.log('clicked');
+        app.addFriend();
+      });
+    },
+    addRoom: function(room) {
+      $('#roomSelect').append("<span>"+room+"</span>");
+    },
+    addFriend: function() {
+    },
+    handleSubmit: function() {
+      $('#send').click(function() {
+        console.log("submitted");
+        var inputMessage = document.getElementById('inputField');
+        app.addMessage({username: 'peter', text: inputMessage.value});
+        inputMessage = '';
+      });
+    }
+  };
 
+var test = function(){
+  console.log("test");
+}
+
+$(document).ready(function() {
+  app.init();
+})
+
+
+
+
+// var message = 'THIS IS WRONG!';
